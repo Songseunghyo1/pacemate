47425a2b3ebf18b5dcd9bdbb247b4f586a9fde56
Songseunghyo1 <tmdgy15@gmail.com>
2021-03-14 23:56:15 +0900

    init

---
+++ src/main/java/com/pacemate/pacemate/PacemateApplication.java
@@ -0,0 +1,13 @@
+package com.pacemate.pacemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacemateApplication.class, args);
+	}
+
+}
