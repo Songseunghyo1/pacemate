4cfa21375d698234d66015009cad9b3ca9818e9b
Songseunghyo1 <tmdgy15@gmail.com>
2023-03-02 22:49:41 +0900

    init

---
+++ src/main/java/com/afterschool/selfstudy/pacemate/PacemateApplication.java
@@ -0,0 +1,13 @@
+package com.afterschool.selfstudy.pacemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afterschool/selfstudy/pacemate/PacemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afterschool.selfstudy.pacemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
